989c30262340eaf73df482d0255b27880602461e
philip <yyin@ualberta.ca>
2017-04-27 11:17:13 -0600

    changes

--- 1-level-python/174/lab6.py
+++ 1-level-python/174/lab6.py
@@ -1,33 +1,20 @@
-x= str(input("enter the pattern"))
+x = str(input("enter the pattern"))
 
 fin = open('words.txt')
-def match_pattern(word,a):
-    
-    if len(word)==len(a):
+
+
+def match_pattern(word, a):
+
+    if len(word) == len(a):
         index = 0
-        while index<len(a):
-            if a[index]!='_' and a[index]!=word[index]:
+        while index < len(a):
+            if a[index] != '_' and a[index] != word[index]:
                 return False
-            index = index +1
-        return True  
+            index = index + 1
+        return True
+
+
 for line in fin:
     word = line.strip()
-    if match_pattern(word,x) == True:
+    if match_pattern(word, x) == True:
         print(word)
-
-                
-                
-            
-
-    
-    
-    
-    
-    
-
-
-
-
-
-
-  
--- 1-level-python/174/lab8.py
+++ 1-level-python/174/lab8.py
@@ -1,20 +1,14 @@
-wordlist=['The','quick','brown','fox','jumps','over','a','lazy','dog']
-a= str(input('Enter a letter:',))
-m=[]
-n=[]
+wordlist = ['The', 'quick', 'brown', 'fox',
+            'jumps', 'over', 'a', 'lazy', 'dog']
+a = str(input('Enter a letter:',))
+m = []
+n = []
 for i in range(len(wordlist)):
-                                                
-                                                if a in wordlist[i]:
-                                                                                                m.append(wordlist[i])
-                                                else:
-                                                                                                n.append(wordlist[i])
-                                                                                               
-                                                
-      
+
+    if a in wordlist[i]:
+        m.append(wordlist[i])
+    else:
+        n.append(wordlist[i])
+
 print (m)
 print(n)
-
-
-
-
-
